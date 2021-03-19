6e4187bae2eb3178f888d0610303737461940a5e
Lucca Honorio <luccahmaaraujo1@gmail.com>
2021-03-19 12:02:29 -0300

    chapter 2 completed

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logoImg from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+interface HeaderProps{
+  OnOpenNewTransactionModal: () =>void;
+}
+
+export function Header({OnOpenNewTransactionModal}:HeaderProps){
+  
+
+  return(
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money"/>
+        <button type="button" onClick={OnOpenNewTransactionModal}>
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  )
+}
